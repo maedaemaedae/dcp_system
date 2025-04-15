e09f3231e5d56c3ae01241f2fd4086f0dcb185e2
Kayla Austria <mkmaustria@tip.edu.ph>
2025-04-15 16:47:06 +0800

    added school list nav option

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,6 +23,12 @@
                         </x-nav-link>
                     @endif
                 </div>
+                
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('schools.index')" :active="request()->routeIs('schools.*')">
+                        {{ __('School List') }}
+                    </x-nav-link>
+                </div>
 
             </div>
 
